b1acadfd2a7641cfd621fc021bf3e1c9e6d8e866
KJH <wo7474@naver.com>
2022-06-21 17:16:07 +0900

    11

--- app/src/main/java/com/example/translation/MainActivity.kt
+++ app/src/main/java/com/example/translation/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_manage
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_manage
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
